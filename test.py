e089b17318a460d811a3cf01a6139e06e9ee3187
A1d3r <a1d3r13@gmail.com>
2025-05-13 16:47:21 -0400

    Auto push from run_odds_api at 20250513_164721

--- test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/merged_game_props.csv")
-
-print("Pitcher props column types:")
-print(df["pitcher_props"].apply(type).value_counts())
-
-print("\nFirst few values:")
-print(df["pitcher_props"].head(5))
